c202b8e41c9b71e9206090232c29e6dc6207dbef
Chirag Rawat <chirag.rawat83@gmail.com>
2022-08-14 12:52:09 +0530

    Toolbar setup complete

--- app/src/main/java/com/example/projectmanager/MainActivity.kt
+++ app/src/main/java/com/example/projectmanager/MainActivity.kt
@@ -3,11 +3,31 @@ package com.example.projectmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.projectmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolBar)
+
+        val actionBar = supportActionBar
+
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val findNavController = this.findNavController(R.id.myNavHostFragment)
+        return findNavController.navigateUp()
     }
 }
