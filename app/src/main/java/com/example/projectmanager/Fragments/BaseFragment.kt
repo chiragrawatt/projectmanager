15c9588e19aaa080e8be8ff5b4744eeb2560ec71
Chirag Rawat <chirag.rawat83@gmail.com>
2022-08-31 12:57:30 +0530

    Added Base Fragment with showProgressBarFunction

---
+++ app/src/main/java/com/example/projectmanager/Fragments/BaseFragment.kt
@@ -0,0 +1,35 @@
+package com.example.projectmanager.Fragments
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.projectmanager.R
+import com.example.projectmanager.databinding.DialogProgressBinding
+
+class BaseFragment : Fragment() {
+
+    private lateinit var mProgressDialog: Dialog
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_base, container, false)
+    }
+
+    fun showProgressDialog(text: String) {
+        mProgressDialog = Dialog(requireContext())
+
+        val mProgressDialogBinding = DialogProgressBinding.inflate(layoutInflater)
+
+        mProgressDialog.setContentView(mProgressDialogBinding.root)
+
+        mProgressDialogBinding.tvProgressText.text = text
+
+        mProgressDialog.show()
+    }
+}
--- app/src/main/java/com/example/projectmanager/MainActivity.kt
+++ app/src/main/java/com/example/projectmanager/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
         if(actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
         }
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
